94e51bdae3d2cf45c9b163c72a1943e84628ead9
Bodawen <qwertyhotwen@gmail.com>
2017-11-10 12:49:07 +0100

    test

--- RechnerNetze/src/Main.java
+++ RechnerNetze/src/Main.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+
+import Client.Client;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("123");
+				Client c = new Client(110,"pop3.126.com");
+				
+				try {
+					c.connection();
+					System.out.println(c.getInfo());
+					c.anmelden("qwertyhot@126.com", "ll");
+					System.out.println(c.getInfo());
+					
+					
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
 
 }
